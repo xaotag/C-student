8ef32539704306d90d5ec257e7f99a621b88e953
xaotag <3036905349@qq.com>
2020-09-29 17:17:21 +0800

    使用 OperaterBase 工具类重构Sql操作

--- studentDetail.aspx.cs
+++ studentDetail.aspx.cs
@@ -17,23 +17,18 @@ namespace WebApplication4
 
         }
 
- 
+
         protected void Button1_Click1(object sender, EventArgs e)
         {
-            //查询
-            string con = ConfigurationManager.ConnectionStrings["xaotag"].ConnectionString;
-            SqlConnection sct = new SqlConnection(con);
-            string sql = "SELECT   classId, studentName, studentNum, studentSex, mobile, password,birthday, province, city, district, isDelete from studentInfo;";
-            SqlDataAdapter sqlData = new SqlDataAdapter(sql, sct);
-            DataSet ds = new DataSet();
-            sqlData.Fill(ds);
+            string sql = "SELECT  classId, studentName, studentNum, studentSex, mobile, password,birthday, province, city, district, isDelete from studentInfo;";
+            DataSet ds = OperaterBase.GetData(sql);
             GridView1.DataSource = ds.Tables[0].DefaultView;
             GridView1.DataBind();
         }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
-            string studentName = TextBox9.Text; 
+            string studentName = TextBox9.Text;
             string studentNum = TextBox1.Text;
             string studentSex = TextBox2.Text;
             string Mobile = TextBox3.Text;
@@ -42,19 +37,12 @@ namespace WebApplication4
             string province = TextBox6.Text;
             string city = TextBox7.Text;
             string district = TextBox8.Text;
-            string studentClassId = TextBox10.Text ;
-            
-            string sql = @"INSERT INTO studentInfo (studentName,studentNum,studentSex,mobile,password,birthday,province,city,district,classId) VALUES (N'" + studentName + "',N'" + studentNum + "',N'" + studentSex + "',N'" + Mobile + "',N'" 
+            string studentClassId = TextBox10.Text;
+
+            string sql = @"INSERT INTO studentInfo (studentName,studentNum,studentSex,mobile,password,birthday,province,city,district,classId) VALUES (N'" + studentName + "',N'" + studentNum + "',N'" + studentSex + "',N'" + Mobile + "',N'"
 + pwd + "','" + birthday + "',N'" + province + "',N'" + city + "',N'" + district + "', N'" + studentClassId + "')";
 
-
-            string con = ConfigurationManager.ConnectionStrings["xaotag"].ConnectionString;
-            SqlConnection sct = new SqlConnection(con);
-            SqlCommand command = new SqlCommand(sql, sct);
-            sct.Open();
-            int flag = command.ExecuteNonQuery();
-            sct.Close();
-            if (flag > 0)
+            if (OperaterBase.commitBySql(sql) > 0)
             {
                 Label11.Text = "插入成功";
             }
