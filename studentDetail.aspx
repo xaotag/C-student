808dd8913c6a93fb50433924746e12c293f13035
xaotag <3036905349@qq.com>
2020-10-06 16:21:27 +0800

    完善 ： 更新 和删除方法

--- studentDetail.aspx
+++ studentDetail.aspx
@@ -37,10 +37,10 @@
                 <asp:Label ID="Label7" runat="server" Text="城市"></asp:Label><asp:TextBox ID="TextBox7" runat="server"></asp:TextBox><br />
                 <asp:Label ID="Label8" runat="server" Text="详细信息"></asp:Label><asp:TextBox ID="TextBox8" runat="server"></asp:TextBox><br />
 
-                <asp:Label ID="Label10" runat="server" Text="学生编号"></asp:Label><asp:TextBox ID="TextBox10" runat="server"></asp:TextBox><br />
+                <asp:Label ID="Label10" runat="server" Text="班级编号"></asp:Label><asp:TextBox ID="TextBox10" runat="server"></asp:TextBox><br />
 
-                <asp:Button ID="Button2" runat="server" Text="插入" OnClick="Button2_Click" /><asp:Label ID="Label11" runat="server"></asp:Label>
-                <div></div>
+                <asp:Button ID="Button2" runat="server" Text="插入" OnClick="Button2_Click" CommandName="insert" /><asp:Label ID="Label11" runat="server"></asp:Label>
+                
             </div>
         </div>
     </form>
--- studentDetail.aspx.cs
+++ studentDetail.aspx.cs
@@ -14,9 +14,22 @@ namespace WebApplication4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-          
+            if (!IsPostBack)
+            {
+                int ID = Convert.ToInt32(Request["ID"]);
+                if (ID > 0)
+                {
+                    Button2.Text = "updata";
+                    fillData(ID);
+                }
+            }
         }
         protected void Button2_Click(object sender, EventArgs e)
+        {
+            addUser();
+        }
+
+        private void addUser()
         {
             string studentName = TextBox9.Text.Trim();
             string studentNum = TextBox1.Text.Trim();
@@ -27,27 +40,40 @@ namespace WebApplication4
             string province = TextBox6.Text.Trim();
             string city = TextBox7.Text.Trim();
             string district = TextBox8.Text.Trim();
+            string classId = TextBox10.Text.Trim();
 
-
-            string sql = @"INSERT INTO studentInfo (studentName,studentNum,studentSex,mobile,password,birthday,province,city,district) VALUES  (N'" + studentName + "',N'" + studentNum + "',N'" + studentSex + "',N'" + Mobile + "',N'"
-+ pwd + "','" + birthday + "',N'" + province + "',N'" + city + "',N'" + district + "')";
-            //判断手机号是否唯一
-            if (IsMobile(studentNum, Mobile))
+            if (Button2.Text == "insert")
             {
-
-                if (OperaterBase.commitBySql(sql) > 0)
+                string sql = @"INSERT INTO studentInfo (studentName,studentNum,studentSex,mobile,password,birthday,province,city,district,classId) VALUES  (N'" + studentName + "',N'" + studentNum + "',N'" + studentSex + "',N'" + Mobile + "',N'"
++ pwd + "','" + birthday + "',N'" + province + "',N'" + city + "',N'" + district + "', N'" + classId + "')";
+                //判断手机号是否唯一
+                if (IsMobile(studentNum, Mobile))
                 {
-                    Label11.Text = "插入成功";
-                    Response.Redirect("studentInfo.aspx");
+
+                    if (OperaterBase.commitBySql(sql) > 0)
+                    {
+                        Label11.Text = "插入成功";
+                        Response.Redirect("studentInfo.aspx");
+                    }
+                }
+                else
+                {
+                    Label11.Text = "插入失败 。。。。";
+
                 }
             }
-            else
+            else if (Button2.Text == "updata")
             {
-                Label11.Text = "插入失败 。。。。";
-
+                // 更新数据
+                string upData = "update studentInfo set studentName = N'" + studentName + "', studentNum = N'" + studentNum + "' ,studentSex = N'" + studentSex + "', mobile = N'" + Mobile + "' , password = N'" + pwd + "', birthday = N'" + birthday + "',pro" +
+                    "vince = N' "+province+"', city = N'" + city + "',district = N'" + district + " ', classId = 'N" + classId + "'";
+                if (OperaterBase.commitBySql(upData) > 0)
+                {
+                    Label11.Text = "修改成功";
+                }
             }
-        }
 
+        }
         /// <summary>
         /// 判断手机号是否唯一
         /// </summary>
@@ -56,13 +82,33 @@ namespace WebApplication4
         private Boolean IsMobile(string studentNum, string mobile)
         {
 
-            string sql = "select mobile , studentNum from studentInfo where  studentNum = '" + studentNum + "' or  mobile = '" +mobile+"'";
+            string sql = "select mobile , studentNum from studentInfo where  studentNum = '" + studentNum + "' or  mobile = '" + mobile + "'";
             DataSet ds = OperaterBase.GetData(sql);
             if (ds.Tables[0].Rows.Count <= 0)
             {
                 return true;
             }
             return false;
+        }
+
+        /// <summary>
+        /// 把数据 填入框里面
+        /// </summary>
+        /// <param name="StudentID"></param>
+        private void fillData(int StudentID)
+        {
+            string Bysql = "select * from studentInfo where ID = " + StudentID;
+            DataSet ds = OperaterBase.GetData(Bysql);
+            TextBox1.Text = ds.Tables[0].Rows[0]["studentNum"].ToString();
+            TextBox2.Text = ds.Tables[0].Rows[0]["studentSex"].ToString();
+            TextBox3.Text = ds.Tables[0].Rows[0]["mobile"].ToString();
+            TextBox4.Text = ds.Tables[0].Rows[0]["password"].ToString();
+            TextBox5.Text = ds.Tables[0].Rows[0]["birthday"].ToString();
+            TextBox6.Text = ds.Tables[0].Rows[0]["province"].ToString();
+            TextBox7.Text = ds.Tables[0].Rows[0]["city"].ToString();
+            TextBox8.Text = ds.Tables[0].Rows[0]["district"].ToString();
+            TextBox10.Text = ds.Tables[0].Rows[0]["classId"].ToString();
+            TextBox9.Text = ds.Tables[0].Rows[0]["studentName"].ToString();
         }
 
 
