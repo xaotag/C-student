84b34425d4a612b58d75ed2d8fc01c03f9c3379a
王唐次 <xaotag@gmail.com>
2020-10-12 21:22:43 +0800

    :sparkles: 修改密码实现

---
+++ Reset.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Reset.aspx.cs" Inherits="WebApplication4.Reset" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+
+</head>
+<body>
+    <form id="form1" runat="server" >
+        <div style="">
+            <asp:Label ID="Label1" runat="server" Text="手机号"></asp:Label><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            <asp:Label ID="Label2" runat="server" Text="密 - 码"></asp:Label><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+            <asp:Button ID="Button1" runat="server" Text="重置" OnClick="Button1_Click1" />
+            <asp:Label ID="Label3" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Reset.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class Reset : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            string mobile = TextBox1.Text.Trim();
+            string password = TextBox2.Text.Trim();
+            if (isMobile(mobile, password))
+            {
+                Label3.Text = "修改成功";
+                return;
+            }
+            Label3.Text = "修改失败";
+
+
+        }
+
+        private Boolean isMobile(string mobile,string password)
+        {
+            if (mobile != "")
+            {
+                string sql = "select mobile from studentInfo where mobile = '" + mobile + "'";
+                if (OperaterBase.GetData(sql)!=null)
+                {
+                    ResetPassword( mobile,  password);
+                    return true;
+                }
+
+                
+
+            }
+
+            return false;
+
+
+
+        }
+
+        private void ResetPassword(string mobile, string password)
+        {
+            if (password!="")
+            {
+                string sql = "update studentInfo set password = '" + password + "' where mobile = '" + mobile + "'";
+                OperaterBase.commitBySql(sql);
+            }
+        }
+
+    }
+}
